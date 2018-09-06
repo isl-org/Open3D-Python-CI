3c636aba0b63daaebed84bd8a47add77a2e2b097
Yixing Lao <yixing.lao@gmail.com>
2018-09-05 17:55:47 -0700

    explicit versions

--- install_python.sh
+++ install_python.sh
@@ -9,13 +9,17 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     HOMEBREW_NO_AUTO_UPDATE=1 brew install pyenv
 
     case "${TOXENV}" in
+        py27)
+            pyenv install 2.7.15
+            pyenv global system 2.7.15
+            ;;
         py35)
-            pyenv install 3.5
-            pyenv global system 3.5
+            pyenv install 3.5.6
+            pyenv global system 3.5.6
             ;;
         py36)
-            pyenv install 3.6
-            pyenv global system 3.6
+            pyenv install 3.6.5
+            pyenv global system 3.6.5
             ;;
     esac
 else
